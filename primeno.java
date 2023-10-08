bb6bf8fe533bd2790c1f93d07dc3e86e4030a37e
sdghate <145231145+sdghate@users.noreply.github.com>
2023-10-08 16:19:56 +0530

    Create primeno.java
    to check prime number or not

---
+++ primeno.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input a number from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        boolean isPrime = isPrimeNumber(number);
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrimeNumber(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        
+        if (number <= 3) {
+            return true;
+        }
+        
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
