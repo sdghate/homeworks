2a12aa16a1dfc5bf22f3a60543b7a0a7dddf5635
sdghate <145231145+sdghate@users.noreply.github.com>
2023-10-21 02:54:14 +0530

    Create obj.java

---
+++ obj.java
@@ -0,0 +1,69 @@
+// Hi Vaibhav, I have created all classes in the same java file Obj.java
+
+class Obj
+  {}
+
+class Human{
+  int h;
+  public static void main(String[] args){
+    Human h1=new Human();
+  }
+}
+
+class Laptop{
+  int l;
+  public static void main(String[] args){
+    Laptop l1=new Laptop();
+  }
+}
+
+class SportMan{
+  int s;
+  public static void main(String[] args){
+    SportMan sm1=new SportMan();
+  }
+}
+class TestCase{
+  int t;
+  public static void main(String[] args){
+    TestCase tc1=new TestCase();
+  }
+}
+
+// 5 other classes of own choice
+
+class Cricket{
+  int c;
+  public static void main(String[] args){
+    Cricket c1=new Cricket();
+  }
+}
+
+class Football{
+  int f;
+  public static void main(String[] args){
+    Football f1=new Football();
+  }
+}
+
+class Hockey{
+  int h;
+  public static void main(String[] args){
+    Hockey h1=new Hockey();
+  }
+}
+
+class Tennis{
+  int t;
+  public static void main(String[] args){
+    Tennis t1=new Tennis();
+  }
+}
+
+class Chess{
+  int c;
+  public static void main(String[] args){
+    Chess c1=new Chess();
+  }
+}
+
